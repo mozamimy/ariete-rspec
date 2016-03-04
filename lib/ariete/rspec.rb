2c1f4b91966742f9184560ccc6c04f10e4551005
Moza USANE <mozamimy@quellencode.org>
2016-03-04 19:40:10 +0900

    Implement matchers of RSpec :tada:

--- lib/ariete/rspec.rb
+++ lib/ariete/rspec.rb
@@ -1,7 +1,15 @@
-require "ariete/rspec/version"
+require 'rspec'
+require 'ariete'
+require 'ariete/rspec/version'
 
-module Ariete
-  module Rspec
-    # Your code goes here...
+RSpec::Matchers.define :be_output do |expect|
+  match do |actual|
+    Ariete.capture_stdout { actual.call } == expect.to_s
+  end
+end
+
+RSpec::Matchers.define :be_output_to_stderr do |expect|
+  match do |actual|
+    Ariete.capture_stderr { actual.call } == expect.to_s
   end
 end
--- spec/ariete/rspec_spec.rb
+++ spec/ariete/rspec_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
+require 'ariete'
 
 describe Ariete::Rspec do
+  def output
+    puts "Ariete is a kind of rabbit."
+    warn "Ariete means 'Lop' in Italian."
+  end
+
   it 'has a version number' do
     expect(Ariete::Rspec::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "custom matchers" do
+    describe "#be_output" do
+      subject { method(:output).to_proc }
+      it { is_expected.to be_output "Ariete is a kind of rabbit.\n" }
+    end
+
+    describe "#be_output_to_stderr" do
+      subject { method(:output).to_proc }
+      it { is_expected.to be_output_to_stderr "Ariete means 'Lop' in Italian.\n" }
+    end
   end
 end
